32f224b46f141729b8516c963fbac7af5906d72c
elyorbek <mengturayevelyorbek@gmail.com>
2026-04-08 13:35:13 +0500

    3

--- src/porla-landing.jsx
+++ src/porla-landing.jsx
@@ -38,17 +38,17 @@ const features = [
     desc: "Qisqa video darslar orqali, tanangizni tushuning Tanangizni tushunish orqali, o’zligingizni anglang",
   },
   {
-    icon: '/Untitled(1)/doctor-female1.png',
+    icon: '/public/svg/Landing page icons/svg1.svg',
     title: "Qulay kuzatuv",
     desc: "Menstrual siklingizni qulay tarzda kuzating va muhim kunlarni oldindan biling.",
   },
   {
-    icon: '/Untitled(1)/medicines1.png',
+    icon: '/public/svg/Landing page icons/svg2.svg',
     title: "Shaxsiylashtirilgan yondashuv",
     desc: "Siz kiritgan ma’lumotlarga qarab sog‘lig‘ingizni yaxshiroq tushunishga yordam beradigan tajriba.",
   },
   {
-    icon: '/Untitled(1)/book1.png',
+    icon: '/public/svg/Landing page icons/svg3.svg',
     title: "Kunlik Maslahatlar",
     desc: "Oy davomida siklingiz davriga mos tarzda kunlik maslahatlar oling.",
   },
